c26d0175887a0a563d6b9cf6b09d61c5cdfe88ed
Sourav <souravc492@gmail.com>
2024-08-16 23:09:20 +0530

    output to public

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,7 @@ module.exports = {
     entry: './src/index.js',
 
     output: {
-        path: path.join(__dirname,'/dist'),
+        path: path.join(__dirname,'/public'),
         filename: 'bundle.js'
 
     },
